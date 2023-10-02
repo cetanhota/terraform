7484e819d260f4fc853bc2cc19ed396d0b56eac9
cetanhota <wayne.leutwler@gmail.com>
2023-10-02 19:36:07 -0400

    new users connection

--- mysql-gcp/main.tf
+++ mysql-gcp/main.tf
@@ -1,12 +1,12 @@
 provider "google" {
-  project     = "zinc-primer-321323"
-  region      = "us-east1"  # Change to your desired region
+  project = "zinc-primer-321323"
+  #region      = "us-east1"  # Change to your desired region
 }
 
 resource "random_password" "mysqldba_pwd" {
-  length = 8
-  min_upper = 1
-  min_lower = 1
+  length      = 8
+  min_upper   = 1
+  min_lower   = 1
   min_numeric = 1
   min_special = 1
 }
@@ -14,22 +14,31 @@ resource "random_password" "mysqldba_pwd" {
 resource "google_sql_database_instance" "weather-srv1" {
   name             = "weather-srv1"
   database_version = "MYSQL_8_0"
-  region           = "us-east1"  # Change to your desired region
+  region           = "us-east1" # Change to your desired region
   settings {
-    #tier = "db-n1-standard-1"  # Change to your desired machine type
     tier = "db-f1-micro"
+    ip_configuration {
+      authorized_networks {
+        name  = "ClientAllowIP"
+        value = "76.34.99.14"
+      }
+    }
+    database_flags {
+      name  = "max_connections"
+      value = 100
+    }
   }
 }
 
 resource "google_sql_user" "admin_user" {
   name     = "mysqldba"
   instance = google_sql_database_instance.weather-srv1.name
-  password = random_password.mysqldba_pwd.result  # Replace with your desired user password
+  password = random_password.mysqldba_pwd.result # Replace with your desired user password
 }
 
 resource "google_sql_database" "db_name" {
-    name = "sbtest"
-    instance = google_sql_database_instance.weather-srv1.name
+  name     = "sbtest"
+  instance = google_sql_database_instance.weather-srv1.name
 }
 
 output "password" {
